81cd8fb7cbab0b4c53af8158fa33c271d4bd790d
salmaakthermim <salmaakthermim112@gmail.com>
2024-11-01 11:26:36 +0600

    navbar setup

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h2 className='text-4xl'>Disum Disum dashing board</h2>
+        </div>
+    );
+};
+
+export default Dashboard;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,25 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Root from './Components/Root/Root';
+import ErrorPage from './Components/ErrorPage/ErrorPage';
+import Home from './Components/Home/Home';
+import Dashboard from './Components/Dashboard/Dashboard';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {path: '/',
+      element: <Home></Home>
+    },
+    {
+      path: 'dashboard',
+      element: <Dashboard></Dashboard>
+    }
+     
+    ]
   },
 ]);
 
